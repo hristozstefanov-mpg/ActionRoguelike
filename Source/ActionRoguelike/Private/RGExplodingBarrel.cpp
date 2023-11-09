ddb6a1eeaa3d223d10fbc87f304b6edf5578bb5b
Hristoz Stefanov <hristozstefanov@mpg.io>
2023-11-09 16:58:47 +0000

    Add exploding barrel
    * Add exploding barrel C++ class and BP
    * When hit by anything it explodes - activating radial force and particle effect
    * Change the projectiles collision profile to hit physics actors (barrel)

---
+++ Source/ActionRoguelike/Private/RGExplodingBarrel.cpp
@@ -0,0 +1,44 @@
+#include "RGExplodingBarrel.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+ARGExplodingBarrel::ARGExplodingBarrel()
+{
+	PrimaryActorTick.bCanEverTick = true;	
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	StaticMeshComponent->SetCollisionProfileName("PhysicsActor");
+	StaticMeshComponent->SetSimulatePhysics(true);
+	RootComponent = StaticMeshComponent;
+
+	RadialForceComponent = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComponent"));
+	RadialForceComponent->ImpulseStrength = 2000.0;
+	RadialForceComponent->bImpulseVelChange = true;
+	RadialForceComponent->Radius = 1000.0f;
+	RadialForceComponent->SetupAttachment(StaticMeshComponent);
+
+	EffectComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ExplosionEffectComponent"));
+	EffectComponent->SetAutoActivate(false);
+	EffectComponent->SetupAttachment(StaticMeshComponent);
+}
+
+void ARGExplodingBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(StaticMeshComponent);
+	StaticMeshComponent->OnComponentHit.AddDynamic(this, &ARGExplodingBarrel::OnHitCallback);
+}
+
+void ARGExplodingBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARGExplodingBarrel::OnHitCallback(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForceComponent->FireImpulse();
+	EffectComponent->Activate(true);
+}
---
+++ Source/ActionRoguelike/Public/RGExplodingBarrel.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RGExplodingBarrel.generated.h"
+
+UCLASS()
+class ARGExplodingBarrel : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	/** Static mesh of the barrel. */
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* StaticMeshComponent;
+
+	/** Radial force of the explosion. */
+	UPROPERTY(VisibleAnywhere)
+	class URadialForceComponent* RadialForceComponent;
+
+	/** Explosion effect. */
+	UPROPERTY(VisibleAnywhere)
+	class UParticleSystemComponent* EffectComponent;
+
+public:
+
+	ARGExplodingBarrel();
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnHitCallback(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+};
