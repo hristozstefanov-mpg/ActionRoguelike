1bf6b88ceb6df89c0c0cca967e86145721c0d8fe
Hristoz Stefanov <hristozstefanov@mpg.io>
2023-11-22 12:45:03 +0000

    Add health widget
    * Create a health widget and add it to the viewport
    * Add an event that gets broadcasted when health changes

--- Source/ActionRoguelike/Private/RGAttributesComponent.cpp
+++ Source/ActionRoguelike/Private/RGAttributesComponent.cpp
@@ -9,5 +9,7 @@ bool URGAttributesComponent::ApplyHealthChange(float Delta)
 {
 	Health += Delta;
 
+	OnHealthChanged.Broadcast(nullptr, this, Health, Delta);
+
 	return true;
 }
--- Source/ActionRoguelike/Public/RGAttributesComponent.h
+++ Source/ActionRoguelike/Public/RGAttributesComponent.h
@@ -5,10 +5,18 @@
 #include "RGAttributesComponent.generated.h"
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FRGOnHealthChanged, AActor*, InstigatorActor, URGAttributesComponent*, OwningComponent, float, NewHealth, float, Delta);
+
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ACTIONROGUELIKE_API URGAttributesComponent : public UActorComponent
 {
 	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FRGOnHealthChanged OnHealthChanged;
 
 protected:
 
